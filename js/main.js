941f40313c6ab60731657db8e8d9c7e921311f51
srivyshnavi93 <vyshuvarsha@gmail.com>
2018-05-13 09:08:35 -0500

    Add files via upload
    Fourth Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+    $(window).scroll(function(){
+        
+        if ($(window).scrollTop() > 970) {
+            $("navbar").addClass("navbar-fixed-top");
+        }
+        
+        
+    });  
+});
